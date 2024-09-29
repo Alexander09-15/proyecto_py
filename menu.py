73a4bcaf7ef9b3850da09e086859034aa57e81e7
fernando <fernanbertidex@gmail.com>
2024-09-29 19:21:44 -0300

    codigo para poner calorias

--- menu.py
+++ menu.py
@@ -1,1 +1,11 @@
-print("hola para el que esta viendo esto uwu")
+from calo import calcular_macros
+def menu():
+    print(f"""
+        FUNCIONES
+          1.calcular tus calorias necesarias.
+""")
+
+    opcion = input("Elige una opcion... > ")
+    if opcion == "1":
+        calcular_macros()
+menu()    
